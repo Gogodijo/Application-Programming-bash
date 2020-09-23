66513ff7c5df9208f46084713dcb27a4088e807e
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2020-09-23 20:55:51 +0300

    Update runPgAdmin.sh

--- runPgAdmin.sh
+++ runPgAdmin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 FILE=/home/ap/pgAdmin4/pgAdmin4/pgadmin4-4.20
+source /home/ap/pgAdmin4/pgAdmin4/bin/activate
 if test -d "$FILE"; then
 	echo "Already unpacked."
 else 
 	wheel unpack /home/ap/pgAdmin4/pgAdmin4/pgadmin4-4.20-py2.py3-none-any.whl -d /home/ap/pgAdmin4/pgAdmin4/
 	sudo chmod 777 /var/lib/
+	sudo chmod 777 /var/log/
 fi
-source /home/ap/pgAdmin4/pgAdmin4/bin/activate
 python /home/ap/pgAdmin4/pgAdmin4/pgadmin4-4.20/pgadmin4/pgAdmin4.py
